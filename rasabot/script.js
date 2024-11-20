598928d5a95f2786f2c09be6d8737531a6cd9457
KennethSAIT <141050311+yazakazukiSAIT@users.noreply.github.com>
2024-11-19 21:51:31 -0700

    Update script.js

--- rasabot/script.js
+++ rasabot/script.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (message) {
             addMessageToChat('User', message);
 
-            fetch("https://1931-34-130-104-142.ngrok-free.app/webhooks/rest/webhook", {
+            fetch("https://497f-34-130-104-142.ngrok-free.app/webhooks/rest/webhook", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -117,11 +117,10 @@ document.addEventListener('DOMContentLoaded', function() {
     function handleTopicSelection(topic) {
         addMessageToChat('User', topic); // Display selected topic as user message
 
-        fetch("https://1931-34-130-104-142.ngrok-free.app/webhooks/rest/webhook", {
+        fetch("https://497f-34-130-104-142.ngrok-free.app/webhooks/rest/webhook", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Origin': 'https://renounding.github.io',
                 'Access-Control-Allow-Origin': '*'
             },
             body: JSON.stringify({ sender: 'user', message: topic })

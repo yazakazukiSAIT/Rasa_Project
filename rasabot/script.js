49b9184883cf4c2c02430372623868d7a2234bab
KennethSAIT <141050311+yazakazukiSAIT@users.noreply.github.com>
2024-12-02 14:16:57 -0700

    Update script.js

--- rasabot/script.js
+++ rasabot/script.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (message) {
             addMessageToChat('User', message);
 
-            fetch("https://d35a-34-130-104-142.ngrok-free.app/webhooks/rest/webhook", {
+            fetch("https://65d1-34-130-223-35.ngrok-free.app/webhooks/rest/webhook", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -117,7 +117,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function handleTopicSelection(topic) {
         addMessageToChat('User', topic); // Display selected topic as user message
 
-        fetch("https://d35a-34-130-104-142.ngrok-free.app/webhooks/rest/webhook", {
+        fetch("https://65d1-34-130-223-35.ngrok-free.app/webhooks/rest/webhook", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
